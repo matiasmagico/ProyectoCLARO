b343cd0011cc9efe0e3d5456d0cfc0fa55439cf1
matiasmagico <111801490+matiasmagico@users.noreply.github.com>
2022-08-22 19:15:23 -0300

    second commit
    funciona cucumber y cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: [
+      "**/*.feature",
+      "**/fetures/*.feature"
+    ],
+    stepDefinitions: [
+      "cypress/e2e/*.{js,ts}",
+      "cypress/e2e/steps_definitions/*.{js,ts}",
+      "cypress/support/step_definitions/**/*.{js,ts}",
+    ],
+    supportFile: false,
+    setupNodeEvents,
   },
 });
---
+++ cypress/e2e/fetures/api.feature
@@ -0,0 +1,4 @@
+Feature: pi pruebs
+  Scenario: vlidcion pis
+    When el usurio llm l servicio "Prueb"
+    Then vlid ue todo slio ok
