2313850b7c68bb4ff774fe66f2a1c652d9222d6d
matiasmagico <111801490+matiasmagico@users.noreply.github.com>
2022-09-05 13:27:47 -0300

    5/9

--- cypress/e2e/features/api.feature
+++ cypress/e2e/features/api.feature
@@ -1,8 +1,7 @@
 Feature: Validacion Servicios
 
   Scenario Outline: Validate-Bussines
-        Then el usuario desea validar la "<condicion>" del "<numero>" 
-        And si el responce da ERROR emitir mensaje de error
+        Then el usuario desea validar la "<condicion>" del "<numero>"
 
         Examples:
        | numero                                                               | condicion |
@@ -10,3 +9,4 @@ Feature: Validacion Servicios
        | 3425469999                                                           | MOVIL     |
        | pruebaconletras                                                      | ERROR     |
        | 9086930485690348509348590348093859034859034859083405983490583490850  | ERROR     |
+       |   | NULL     |
--- cypress/e2e/step_definitions/api.js
+++ cypress/e2e/step_definitions/api.js
@@ -1,54 +1,34 @@
 import { Then } from "@badeball/cypress-cucumber-preprocessor"
-const data = require("../../fixtures/API.json")
-
+var data = require("../../fixtures/API.json")
+const dataenviroment = require("../../fixtures/dataenviroment/dev.json") 
 
 Then("el usuario desea validar la {string} del {string}", (condicion, numero) => {
+  cy.log("Configuramos los valores a utilizar ")
+  data["validate-business"].request.body.billNumber = numero
 
-  cy.log("Configuramos los valores a utilizar")
-  data["validate-business"].request.body.cellularNumber = numero
-
-  if(numero === "NULL"){
-    cy.log("numero = NULL")
+  if(!numero){
     data["validate-business"].response.status = 400
-    data["validate-bussines"].request.body.cellularNumber = null
   }
 
   if(condicion === "ERROR"){
-    cy.log("condicion = Error")
     data["validate-business"].response.body.business = null
+    data["validate-business"].response.body.error = data["validate-business"].response.default.error
   }else{
-    cy.log("else")
     data["validate-business"].response.body.business = condicion
     data["validate-business"].response.body.error = null
   }
-
-  cy.log("request")
-  cy.log("url: " + data["validate-business"].request.url)
-  cy.log("headers: " + data["validate-business"].request.headers["Content-Type"])
-  cy.log("method: " + data["validate-business"].request.method)
-  cy.log("body: " + data["validate-business"].request.body.cellularNumber)
-  cy.log("response")
-  cy.log("status: " + data["validate-business"].response.status)
-  cy.log("business: " + data["validate-business"].response.body.business)
-  cy.log("error: " + data["validate-business"].response.body.error)
-
+cy.log(data["validate-business"].response.status)
   cy.request({
-    url: data["validate-business"].request.url,
+    url: dataenviroment["validatebusiness"],
     headers: data["validate-business"].request.headers,
     method: data["validate-business"].request.method,
-    body: data["validate-business"].request.body
+    body: JSON.stringify(data["validate-business"].request.body),
+    failOnStatusCode: false
   }).then((response) => {
-    cy.log("body completo b")
-    cy.log(response.body)
     expect(response.status).to.eq(data["validate-business"].response.status)
-    expect(response.body.business).to.eq(data["validate-business"].response.body.business)
-    cy.log(response.body.error)
+    if(response.status === 200){
+      expect(response.body.business).to.eq(data["validate-business"].response.body.business)
+      expect(response.body.error).to.eq(data["validate-business"].response.body.error)
+    }
   })
 })
-
-Then("si el responce da ERROR emitir mensaje de error", ()=> {
-  
-  cy.log(response.body)
-  if (response.body === error)
-  cy.log(response.body.error)
-  })
